4e4c42b548f3f824d7ba02b2468271942acc7134
baozoudashijian <1787009521@qq.com>
2023-05-27 18:04:08 +0800

    feat: swagger和入参校验

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('env2')('./.env') // 位置要放在config前面
 const Hapi = require('hapi')
 const config = require('./config')
 const routesHelloHapi = require('./routes/hello-world')
+const routesShopHapi = require('./routes/shop')
+const routesOrderHapi = require('./routes/order')
+const pluginHapiSwagger = require('./plugins/swagger');
 
 
 const server = new Hapi.Server()
@@ -12,8 +15,13 @@ server.connection({
 })
 
 const init = async () => {
+    await server.register([
+        ...pluginHapiSwagger
+    ])
     server.route([
-        ...routesHelloHapi
+        ...routesHelloHapi,
+        ...routesShopHapi,
+        ...routesOrderHapi
     ])
     await server.start()
     console.log('server is running')
--- routes/hello-world.js
+++ routes/hello-world.js
@@ -1,10 +1,14 @@
+
 module.exports = [
     {
         method: 'GET',
         path: '/',
         handler: (request, reply) => {
             reply('hapi')
+        },
+        config: {
+            tags: ['api', 'test'],
+            description: '测试hello-hapi'
         }
-
     }
 ]
