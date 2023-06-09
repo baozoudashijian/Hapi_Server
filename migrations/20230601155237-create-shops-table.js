3ce4e2fbdf007480e2c5abf88543ac994417dae1
baozoudashijian <1787009521@qq.com>
2023-06-09 22:02:56 +0800

    feat: 完成表设计，表迁移，填充模拟数据和使用vscode插件连接数据库

--- migrations/20230601155237-create-shops-table.js
+++ migrations/20230601155237-create-shops-table.js
@@ -1,22 +1,25 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    queryInterface.createTable("shops", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      thumb_url: Sequelize.STRING,
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    queryInterface.dropTable('shops')
+  },
 };
---
+++ seeders/20230609135630-init-shops.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const timestamps = {
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('shops', [
+      { id: 1, name: '店铺1', thumb_url: '1.png', ...timestamps },
+      { id: 2, name: '店铺2', thumb_url: '2.png', ...timestamps },
+      { id: 3, name: '店铺3', thumb_url: '3.png', ...timestamps },
+      { id: 4, name: '店铺4', thumb_url: '4.png', ...timestamps },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    const { Op } = Sequelize;
+    await queryInterface.bulkDelete('shops', { id: { [Op.in]: [1, 2, 3, 4] } }, {});
+  }
+};
